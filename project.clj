04a2112ab2fa5be9f3875e1788f5c2f559281514
Cymen Vig <cymenvig@gmail.com>
2012-02-22 13:47:55 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-http "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [server-socket "1.0.0"]])
---
+++ test/clojure_http/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-http.test.core
+  (:use [clojure-http.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
