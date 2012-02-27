426d9feb63d1cce183c952ccfd8ee8996b42540a
Cymen Vig <cymenvig@gmail.com>
2012-02-27 10:50:28 -0600

    Listens on port and echoes back request headers

--- src/clojure_http/core.clj
+++ src/clojure_http/core.clj
@@ -1,13 +1,16 @@
 (ns clojure-http.core)
 (import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
 (use 'clojure.contrib.server-socket)
+(use 'clojure.contrib.io)
 (defn http-server []
   (letfn [(http [in out]
                     (binding [*in* (BufferedReader. (InputStreamReader. in))
                               *out* (OutputStreamWriter. out)]
-                      (let [input (read-line)]
-                        (print input)
-                        (flush))))]
+                      (loop [line (read-line)]
+                             (println line)
+                             (if (not (= line ""))
+                               (recur (read-line))))
+                      ))]
     (create-server 5000 http)))
 (defn -main []
   (http-server))
