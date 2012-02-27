8a9e5da8a43024433c9b449f8091002ced2cd63f
Cymen Vig <cymenvig@gmail.com>
2012-02-27 17:06:21 -0600

    Can now parse HTTP request

--- src/clojure_http/core.clj
+++ src/clojure_http/core.clj
@@ -2,15 +2,39 @@
 (import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
 (use 'clojure.contrib.server-socket)
 (use 'clojure.contrib.io)
+(use 'clojure.string)
+
+(defn read-until-empty []
+  (loop [line (read-line) acc []]
+    (if (= line "")
+      acc
+      (recur (read-line) (conj acc line)))))
+
+(defn parse-key-value-into [collection line]
+  (let [pair (rest (re-matches #"([^:]+): (.+)" line))]
+    (assoc collection (keyword (first pair)) (second pair))))
+
+(defn parse-request [request-lines]
+  (let [request
+    (merge
+      (zipmap [:Method, :Request-URI, :HTTP-Version] (split (first request-lines) #"\s+"))
+      (reduce parse-key-value-into {} (rest request-lines)))]
+    (doseq [kv request]
+      (println (first kv) " --> " (second kv)))
+  )
+request-lines)
+
 (defn http-server []
   (letfn [(http [in out]
                     (binding [*in* (BufferedReader. (InputStreamReader. in))
                               *out* (OutputStreamWriter. out)]
-                      (loop [line (read-line)]
-                             (println line)
-                             (if (not (= line ""))
-                               (recur (read-line))))
-                      ))]
+                        (let [request
+                          (parse-request (read-until-empty))]
+                          (println "\nKABOOM!\n")
+                          (println request)
+                        )
+                    ))]
     (create-server 5000 http)))
+
 (defn -main []
   (http-server))
