167a21e8fa48438fe1c43a3d14a906a9ab2e3d5d
Cymen Vig <cymenvig@gmail.com>
2012-03-01 12:01:19 -0600

    Start testing clojure-http.method-get

---
+++ spec/clojure-http/method_get_spec.clj
@@ -0,0 +1,9 @@
+(ns clojure-http.method-get-spec
+  (:use speclj.core
+        clojure-http.method-get))
+
+(describe "method-get"
+  (it "can resolve a request URI to a file within root"
+    (should (= "public/nachos" (resolve-file {:Request-URI "/nachos"})))))
+
+(run-specs)
