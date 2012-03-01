09097e3a9490b23c5868792d2e75111e707f88f7
Cymen Vig <cymenvig@gmail.com>
2012-03-01 16:02:49 -0600

    Remove uneeded (flush) now the in and out streams are being closed

--- src/clojure_http/core.clj
+++ src/clojure_http/core.clj
@@ -11,7 +11,6 @@
               *out* (writer out)]
       (do
         (method (parse-request-headers *in*) out)
-        (flush)
         (. *in* close)
         (. *out* close))))]
     (create-server port http)))
