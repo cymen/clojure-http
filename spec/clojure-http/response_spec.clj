b20e7d5e3e2972a533a695a9447c03b16b28604b
Cymen Vig <cymenvig@gmail.com>
2012-03-01 10:19:43 -0600

    Update clojure-http.response testing to also test time offset (local time zone -> GMT)

--- spec/clojure-http/response_spec.clj
+++ spec/clojure-http/response_spec.clj
@@ -1,14 +1,17 @@
 (ns clojure-http.response-spec
   (:use [speclj.core])
   (:use [clojure-http.response])
-  (:use [clj-time.core :only [date-time]])
+  (:use [clj-time.core :only [date-time from-time-zone time-zone-for-offset]])
   (:use [clj-time.coerce :only [to-long]]))
 
 (describe "response"
   (it "converts UNIX timestamp to HTTP date format"
-    (do
-      (should
-        (= "Tue, 09 Aug 1977 04:15:30 GMT"
-          (datetime-in-gmt (to-long (date-time 1977 8 9 4 15 30))))))))
+    (should
+      (= "Tue, 09 Aug 1977 09:15:30 GMT"
+        (datetime-in-gmt
+          (to-long
+            (from-time-zone
+              (date-time 1977 8 9 4 15 30)
+              (time-zone-for-offset -5))))))))
 
 (run-specs)
