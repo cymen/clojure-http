7b88dfc1481fb6c6273dd2a1b78c95bfb47e9377
Cymen Vig <cymenvig@gmail.com>
2012-03-06 10:53:10 -0600

    Continue prior refactoring: POST now uses hash-map response

--- src/clojure_http/method/post.clj
+++ src/clojure_http/method/post.clj
@@ -1,14 +1,12 @@
 (ns clojure-http.method.post
   (:use clojure-http.method
-        clojure-http.request.parse
-        clojure-http.utility.datetime))
+        clojure-http.request.parse))
 
 (defmethod method :POST [request-headers]
-  (do
-    (println (:HTTP-Version request-headers) "200 OK")
-    (println "Connection: close")
-    (println "Date:" (datetime-in-gmt))
-    (println "Accept-Ranges: none")
-    (println "Server: clip-clop/0.1")
-    (println "")
-    (println (parse-request-body request-headers))))
+  (hash-map
+    :Status-Line {
+      :HTTP-Version (:HTTP-Version request-headers)
+      :Status-Code 200
+      :Status-Message "OK"
+    }
+    :Body (parse-request-body request-headers)))
