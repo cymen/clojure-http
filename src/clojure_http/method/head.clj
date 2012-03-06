b7abf024c91fae0a4fa9cfdd6c1ac17558a7128b
Cymen Vig <cymenvig@gmail.com>
2012-03-06 10:49:29 -0600

    Get HEAD method working

--- src/clojure_http/method/head.clj
+++ src/clojure_http/method/head.clj
@@ -1,5 +1,10 @@
 (ns clojure-http.method.head
   (:use [clojure-http.method]))
 
+(defn- override-method [request-headers]
+  (merge
+    request-headers
+    { :Method "GET" }))
+
 (defmethod method :HEAD [request-headers]
-  (println "HEAD!"))
+  (dissoc (method (override-method request-headers)) :Body))
