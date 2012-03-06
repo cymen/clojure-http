0d59daf13dfdf15c249cf30675452302d221ce01
Cymen Vig <cymenvig@gmail.com>
2012-03-06 12:13:36 -0600

    Adding tests

--- spec/clojure-http/method_get_spec.clj
+++ spec/clojure-http/method_get_spec.clj
@@ -3,7 +3,14 @@
         clojure-http.method.get))
 
 (describe "method-get"
+
   (it "can resolve a request URI to a file within root"
-    (should (= "public/nachos" (resolve-file {:Request-URI "/nachos"})))))
+    (should (= "public/nachos" (resolve-file {:Request-URI "/nachos"}))))
+
+;  (it "responds to a GET request for a file"
+;    (let [filename "/index.html"]
+;      (println (method :GET { Method "GET" :Request-URI filename }))))
+
+)
 
 (run-specs)
